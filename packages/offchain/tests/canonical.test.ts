2d6aaadf064a2ef267b36679c043b1796a3d047d
Cody Butz <codyjbutz@gmail.com>
2026-02-11 11:28:59 -0500

    lint and format

--- packages/offchain/tests/canonical.test.ts
+++ packages/offchain/tests/canonical.test.ts
@@ -1,6 +1,21 @@
-import { addAssets, credentialToAddress, Data, Emulator, EmulatorAccount, generateEmulatorAccount, Lucid, LucidEvolution, TxBuilder, validatorToScriptHash } from '@lucid-evolution/lucid';
+import {
+  addAssets,
+  credentialToAddress,
+  Data,
+  Emulator,
+  EmulatorAccount,
+  generateEmulatorAccount,
+  Lucid,
+  LucidEvolution,
+  TxBuilder,
+  validatorToScriptHash,
+} from '@lucid-evolution/lucid';
 import { beforeEach, expect, it } from 'vitest';
-import { CanonicalDatum, mkValidator, serialiseCanonicalDatum } from '../src/index.js';
+import {
+  CanonicalDatum,
+  mkValidator,
+  serialiseCanonicalDatum,
+} from '../src/index.js';
 
 type EmulatorAccountMap = Record<string, EmulatorAccount>;
 
@@ -10,20 +25,14 @@ type LucidContext<T extends EmulatorAccountMap = EmulatorAccountMap> = {
   emulator: Emulator;
 };
 
-export async function createTestContext(
-  context: LucidContext,
-): Promise<void> {
+export async function createTestContext(context: LucidContext): Promise<void> {
   context.users = {
     user: generateEmulatorAccount(
-      addAssets(
-        {'lovelace': 100_000_000_000_000n},
-      ),
+      addAssets({ lovelace: 100_000_000_000_000n }),
     ),
   };
 
-  context.emulator = new Emulator([
-    context.users.user,
-  ]);
+  context.emulator = new Emulator([context.users.user]);
 
   context.lucid = await Lucid(context.emulator, 'Custom');
   context.lucid.selectWallet.fromSeed(context.users.user.seedPhrase);
@@ -56,15 +65,12 @@ beforeEach<LucidContext>(async (context: LucidContext) => {
 });
 
 it('Non-canonical input vs. canonical output', async (context: LucidContext) => {
-  const {lucid} = context;
+  const { lucid } = context;
   const validator = mkValidator();
-  const scriptAddress = credentialToAddress(
-    lucid.config().network!,
-    {
-      hash: validatorToScriptHash(validator),
-      type: 'Script'
-    }
-  );
+  const scriptAddress = credentialToAddress(lucid.config().network!, {
+    hash: validatorToScriptHash(validator),
+    type: 'Script',
+  });
 
   const canonicalDatum: CanonicalDatum = { some_number: { number: 1n } };
   const datumCanonical = serialiseCanonicalDatum(canonicalDatum, true);
@@ -74,16 +80,27 @@ it('Non-canonical input vs. canonical output', async (context: LucidContext) => 
 
   await runAndAwaitTxBuilder(
     lucid,
-    lucid.newTx().pay.ToContract(scriptAddress, { kind: 'inline', value: datumNonCanonical }, { 'lovelace': 5_000_000n }),
+    lucid
+      .newTx()
+      .pay.ToContract(
+        scriptAddress,
+        { kind: 'inline', value: datumNonCanonical },
+        { lovelace: 5_000_000n },
+      ),
   );
 
   const [utxo] = await lucid.utxosAt(scriptAddress);
 
   await runAndAwaitTxBuilder(
     lucid,
-    lucid.newTx()
+    lucid
+      .newTx()
       .collectFrom([utxo], Data.void())
       .attach.SpendingValidator(validator)
-      .pay.ToContract(scriptAddress, { kind: 'inline', value: datumCanonical }, { 'lovelace': 5_000_000n }),
+      .pay.ToContract(
+        scriptAddress,
+        { kind: 'inline', value: datumCanonical },
+        { lovelace: 5_000_000n },
+      ),
   );
 });
